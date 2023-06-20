6fdaabd2f74f12cc633939cea935012221343472
mckrava <mckrava2@gmail.com>
2023-06-20 09:35:38 +0300

    feat: refactored env vars usage

--- src/config.ts
+++ src/config.ts
@@ -18,18 +18,12 @@ export class xSocialConfig {
   @IsNotEmpty()
   readonly MONGODB_URL: string;
   @IsNotEmpty()
-  readonly ADMIN_IDS: string;
-  @IsNotEmpty()
   readonly GQL_API_AUTH_SECRET: string;
   @IsNotEmpty()
   readonly DATA_PROVIDER_SQUID_WS_URL: string;
 
-  public ADMIN_IDS_LIST: number[];
-
-  public TG_NO_ADMIN_PROTECTION_STR: string;
   public API_NO_ADMIN_PROTECTION_STR: string;
 
-  public TG_NO_ADMIN_PROTECTION = false;
   public API_NO_ADMIN_PROTECTION = false;
 }
 
@@ -40,13 +34,6 @@ export class xSocialConfig {
       provide: xSocialConfig,
       useFactory: () => {
         const env = transformAndValidateSync(xSocialConfig, process.env);
-        env.ADMIN_IDS_LIST = env.ADMIN_IDS.split(';;')
-          .map((id) =>
-            !Number.isNaN(Number.parseInt(id)) ? Number.parseInt(id) : null
-          )
-          .filter((maybeId) => !!maybeId);
-        env.TG_NO_ADMIN_PROTECTION =
-          env.TG_NO_ADMIN_PROTECTION_STR === 'true' || false;
         env.API_NO_ADMIN_PROTECTION =
           env.API_NO_ADMIN_PROTECTION_STR === 'true' || false;
         return env;
